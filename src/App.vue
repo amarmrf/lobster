8aa471129e66bfe92809e8e7bc04f47c868037c3
Amar Ma'ruf <amarmfmd@gmail.com>
2022-01-08 15:19:43 +0700

    starter code

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-if="appReady" class="min-h-full font-Poppins box-border">
+    <Navigation/>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+import { ref } from "vue"
+import { supabase } from "./supabase/init"
+import store from "./store"
+
+export default {
+  components: {
+    Navigation,
+  },
+  setup() {
+    // Create data / vars
+    const appReady = ref(null)
+    // Check to see if user is already logged in
+    const user = supabase.auth.user()
+
+    // If user does not exist, need to make app ready
+    if (!user) {
+      appReady.value = true
+    }
+
+    // Runs when there is a auth state change
+    // this code runs on setup i.e. mounting and check if a user has just newly logged in
+    // everytime we login and push the router to 'Home'
+    // the setup in this App.vue file is run since we re-mount
+    // BUT where _ and session is fed from?
+    supabase.auth.onAuthStateChange((_, session) => {
+      store.methods.setUser(session)
+      appReady.value = true
+    })
+
+    return { appReady };
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap");
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "Home-view",
+  components: {},
+  setup() {
+    // Create data / vars
+
+    // Get data
+
+    // Run data function
+
+    return {};
+  },
+};
+</script>
