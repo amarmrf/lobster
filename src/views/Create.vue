188bda97e057642f494435c568d9c6c12cfbbe14
Amar Ma'ruf <amarmfmd@gmail.com>
2022-01-09 20:09:46 +0700

    add create

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,22 +1,168 @@
 <template>
-  <div></div>
+  <div class="max-w-screen-md mx-auto px-4 py-10">
+    <!-- Status Message -->
+    <div
+      v-if="statusMsg || errorMsg"
+      class="mb-10 p-4 bg-light-grey rounded-md shadow-lg"
+    >
+      <p class="text-at-light-green">
+        {{ statusMsg }}
+      </p>
+      <p class="text-red-500">{{ errorMsg }}</p>
+    </div>
+
+    <!-- Create -->
+    <div class="p-8 flex items-start bg-light-grey rounded-md shadow-lg">
+      <!-- Form -->
+      <form @submit.prevent="createExercise" class="flex flex-col gap-y-5 w-full">
+        <h1 class="text-2xl text-at-light-green">Past Authoring</h1>
+
+        <div class="flex flex-col gap-y-4">
+          <div
+            class="flex flex-col gap-x-6 gap-y-2 relative"
+            v-for="(item, index) in epochs"
+            :key="index"
+          >
+            <div class="flex flex-col ">
+              <label for="epoch-name" class="mb-1 text-sm text-at-light-green"
+                >Epoch {{ index + 1 }} Title
+              </label>
+              <input
+                required
+                type="text"
+                class="p-2 w-full text-gray-500 focus:outline-none"
+                v-model="item.title"
+              />
+            </div>
+            <img
+              @click="deleteEpoch(item.id)"
+              src="@/assets/images/trash-light-green.png"
+              class="h-4 w-auto absolute -left-5 cursor-pointer"
+              alt=""
+            />
+          </div>
+          <button
+            @click="addEpoch"
+            type="button"
+            class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+            text-white bg-at-light-green duration-200 border-solid
+            border-2 border-transparent hover:border-at-light-green hover:bg-white
+            hover:text-at-light-green"
+          >
+            Add Epoch
+          </button>
+        </div>
+
+     
+        <button
+          type="submit"
+          class="mt-6 py-2 px-6 rounded-sm self-start text-sm
+      text-white bg-at-light-green duration-200 border-solid
+      border-2 border-transparent hover:border-at-light-green hover:bg-white
+      hover:text-at-light-green"
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import { uid } from "uid";
+import { supabase } from "../supabase/init";
 export default {
-  name: "create-epoch",
+  name: "create-past",
   setup() {
     // Create data
+    const exerciseName = ref("");
+    const exerciseType = ref("select-exercise");
+    const epochs = ref([]);
+    const statusMsg = ref(null);
+    const errorMsg = ref(null);
+    const dataLoaded = ref(null);
 
-    // Add exercise
+    // Get data
+    const getData = async () => {
+      try {
+        const { data: items, error } = await supabase.from("past_epochs").select("*");
+        if (error) throw error;
+        epochs.value = items;
+        dataLoaded.value = true;
+      } catch (error) {
+        console.warn(error.message);
+      }
+    };
 
-    // Delete exercise
+    // Run data function
+    getData(); 
+    
 
-    // Listens for chaging of workout type input
+    // Add epoch
+    const addEpoch = () => {
+      if (epochs.value.length < 7) {
+        epochs.value.push({
+          id: uid(),
+          title: "",
+          sequence_number: epochs.value.length
+        });
+      }
+    };
 
-    // Create workout
+    // Delete epoch
+    const deleteEpoch = (id) => {
+      if (epochs.value.length > 1) {
+        epochs.value = epochs.value.filter((epoch) => epoch.id !== id);
+        return;
+      }
+      errorMsg.value = "Error: Cannot remove, need to at least have one epoch";
+      setTimeout(() => {
+        errorMsg.value = false;
+      }, 5000);
+    };
 
-    return {};
+    // Listens for chaging of exercise type input
+    const exerciseChange = () => {
+      epochs.value = [];
+      addEpoch();
+    };
+
+    // Create exercise
+    const createExercise = async () => {
+      try {
+        const { error } = await supabase.from("past_epochs").insert([
+          {
+            epochs: epochs.value,
+          },
+        ]);
+        if (error) throw error;
+        statusMsg.value = "Succes: Exercise Created!";
+        exerciseName.value = null;
+        exerciseType.value = "select-exercise";
+        epochs.value = [];
+        setTimeout(() => {
+          statusMsg.value = false;
+        }, 5000);
+      } catch (error) {
+        errorMsg.value = `Error: ${error.message}`;
+        setTimeout(() => {
+          errorMsg.value = false;
+        }, 5000);
+      }
+    };
+
+    return {
+      exerciseName,
+      exerciseType,
+      epochs,
+      statusMsg,
+      errorMsg,
+      addEpoch,
+      exerciseChange,
+      deleteEpoch,
+      createExercise,
+    };
   },
 };
 </script>
